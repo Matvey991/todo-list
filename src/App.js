d5a5ae65c446c1cb6451fbbcd2174005b8936314
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-09-26 23:14:29 +0400

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
-import {Footer} from "./components/Footer/Footer";
-import {Main} from "./components/Main/Main";
-import {Header} from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Main } from "./components/Main/Main";
+import { Header } from "./components/Header/Header";
 import { useTodos } from "./components/customHooks/useTodos";
 
-
 function App() {
+  const { todos, AddNewTodo, DeleteTodo, ChangeTodoStatus, DeleteAllTodos } =
+    useTodos();
 
-  const { todos, AddNewTodo, DeleteTodo, ChangeTodoStatus, DeleteAllTodos } = useTodos()
-  
   return (
     <div>
-  <div className="container py-5">
-<Header AddNewTodo={AddNewTodo} />
-<hr/>
-<Main todos={todos} ChangeTodoStatus={ChangeTodoStatus} DeleteTodo={DeleteTodo} />
-<hr/>
-<Footer DeleteAllTodos={DeleteAllTodos} todos={todos} />
-</div>
-</div>
-);
+      <div className="container py-5">
+        <Header AddNewTodo={AddNewTodo} />
+        <hr />
+        <Main
+          todos={todos}
+          ChangeTodoStatus={ChangeTodoStatus}
+          DeleteTodo={DeleteTodo}
+        />
+        <hr />
+        <Footer DeleteAllTodos={DeleteAllTodos} todos={todos} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
-import Form from './Form/Form';
-import { Modal } from '../Modal/Modal';
+import React, { useState } from "react";
+import Form from "./Form/Form";
+import { Modal } from "../Modal/Modal";
 
-export const Header = ( {AddNewTodo} ) => {
+export const Header = ({ AddNewTodo }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const [isModalOpen, setIsModalOpen] = useState(false)
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const openModal = () => {
-        setIsModalOpen(true)
-    }
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false)
-    }
-
-    return (
-        <header className='d-flex justify-content-center'>
-             <button onClick={openModal} type="submit" className="btn btn-primary">Open modal</button>
-    <Modal closeHandler={closeModal} isOpen={isModalOpen}>
-    <Form closeHandler={closeModal}  AddNewTodo={AddNewTodo} />
-    </Modal>
-        </header>
-    );
+  return (
+    <header className="d-flex justify-content-center">
+      <button onClick={openModal} type="submit" className="btn btn-primary">
+        Open modal
+      </button>
+      <Modal closeHandler={closeModal} isOpen={isModalOpen}>
+        <Form closeHandler={closeModal} AddNewTodo={AddNewTodo} />
+      </Modal>
+    </header>
+  );
 };
