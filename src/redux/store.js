23cef1a1b6182635860a7a25a9b04e9ec0bcb40e
Matvey <matveyfedotov61@gmail.com>
2025-02-20 18:13:27 +0400

    removing redux

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import { rootReducer } from "./reducers/rootReducers";
-
-const initialState = {
-  todos: [],
-  counter: 0,
-};
-
-export const store = createStore(rootReducer, initialState);
--- src/redux/types/todosTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TODO = "ADD_TODO";
-export const DELETE_TODO = "DELETE_TODO";
-export const CHANGE_STATUS_TODO = "CHANGE_STATUS_TODO";
-export const CLEAR_TODOS = "CLEAR_TODOS";
