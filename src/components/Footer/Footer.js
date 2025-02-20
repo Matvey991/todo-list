e03fd267308f75bfc4cdb84ee11c871be7fb9b55
Matvey <matveyfedotov61@gmail.com>
2025-02-20 18:22:30 +0400

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
-export const Footer = React.memo(( {todos, DeleteAllTodos} ) => { 
-    
-    return (
-        <footer className="d-flex flex-column align-items-center">
-    <button 
-        className='btn btn-danger'
-        disabled={!todos.length} 
-        onClick={() => DeleteAllTodos()} >
-            Удалить все
-    </button>
-        </footer>
-    )
-})
+export const Footer = React.memo(({ todos, DeleteAllTodos }) => {
+  return (
+    <footer className="d-flex flex-column align-items-center">
+      <button
+        className="btn btn-danger"
+        disabled={!todos.length}
+        onClick={() => DeleteAllTodos()}
+      >
+        Удалить все
+      </button>
+    </footer>
+  );
+});
